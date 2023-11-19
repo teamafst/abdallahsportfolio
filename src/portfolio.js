a99fdd15cad4ca8939b0b75152e3ed1e03991869
AbdallahAli2 <119799728+AbdallahAli2@users.noreply.github.com>
2023-11-19 15:06:19 +0000

    Lorem ipsum- changed some names

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,9 +19,8 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Abdallah Ali",
+  logo_name: "Abs",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
